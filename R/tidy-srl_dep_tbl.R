8205216a54250e60ad0f9fbf36cfdf211d6f2d40
LeonFernandes <leon.fernandes@columbia.edu>
2023-07-25 22:53:23 -0400

    tidy cleanup

--- R/tidy-srl_dep_tbl.R
+++ R/tidy-srl_dep_tbl.R
@@ -1,28 +1,30 @@
 #' Tidy method for serial dependence
 #'
-#' @param object an `tibble` of class `srl_dep_tbl`.
+#' @inheritParams generics::tidy
+#' @param x a `tibble` of class `srl_dep_tbl`.
 #' @returns a `tibble` with columns `.metric`, `.estimator`, `lag` and
 #'      `.estimate`.
+#' @rdname tidy_serial
 #' @exportS3Method generics::tidy
 tidy.srl_dep_tbl <-
-    function(object) {
-        tidy_srl_dep_impl(object)
+    function(x, ...) {
+        tidy_srl_dep_impl(x)
     }
 
 tidy_srl_dep_impl <-
     function(object) {
-        lag <- rlang::sym("lag")
-        .metric <- rlang::sym(".metric")
-        .estimator <- rlang::sym(".estimator")
-        .estimate <- rlang::sym(".estimate")
+        .lag <- rlang::sym("lag")
+        .metric <- rlang::sym("metric")
+        .estimator <- rlang::sym("estimator")
+        .estimate <- rlang::sym("estimate")
         object |>
             tidyr::pivot_longer(
-                cols = -lag, names_to = ".metric", values_to = ".estimate"
+                cols = -.lag, names_to = ".metric", values_to = ".estimate"
             ) |>
             dplyr::mutate(
                 .estimator = factor("serial_dependence"),
                 .metric = as.factor(.metric)
             ) |>
             dplyr::relocate(.estimator, .before = .estimate) |>
-            dplyr::relocate(lag, .before = .estimate)
+            dplyr::relocate(.lag, .before = .estimate)
     }
